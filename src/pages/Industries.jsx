787da3222363f556269ba884d1bb966a777f3410
lsleewebsite <lsleewebsite@gmail.com>
2026-05-12 15:07:07 +0800

    Create Industries.jsx

---
+++ src/pages/Industries.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Industries() {
+  return (
+    <div style={{ padding: '120px 32px 80px', minHeight: '80vh' }}>
+      <div style={{ maxWidth: '1360px', margin: '0 auto' }}>
+        <h1 style={{ fontSize: '48px', fontWeight: 800, marginBottom: '24px' }}>
+          Industries We Serve
+        </h1>
+        <p style={{ fontSize: '18px', color: '#64748B', marginBottom: '40px' }}>
+          Detailed industry pages coming soon...
+        </p>
+      </div>
+    </div>
+  )
+}
